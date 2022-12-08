f7353d0f414cb4e9352d0f285d09f1925d5e4d61
Larson-Flores <15198510@modelo.edu.mx>
2022-12-08 12:53:46 -0600

    my first commit

---
+++ Practica.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola mundo</h1>
+    <p> 
+        what is the current time and date?
+    </p>
+    <p>
+        your current time and date is:
+    </p>
+    <?php
+    echo date("d.m.Y H:I:S");
+    ?>
+</body>
+</html>
